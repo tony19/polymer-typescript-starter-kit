d53a683bd3ccd5f878969dfdbadb33cb2de6bd8c
Tony Trinh <tony19@gmail.com>
2017-01-07 11:42:12 -0600

    Switch my-view{2,3} to TypeScript

---
+++ src/scripts/my-view2.ts
@@ -0,0 +1,6 @@
+@component('my-view2')
+class MyView2 extends polymer.Base {
+  @property()
+  title = 'View Two';
+}
+MyView2.register();
---
+++ src/scripts/my-view3.ts
@@ -0,0 +1,6 @@
+@component('my-view3')
+class MyView3 extends polymer.Base {
+  @property()
+  title = 'View Three';
+}
+MyView3.register();
