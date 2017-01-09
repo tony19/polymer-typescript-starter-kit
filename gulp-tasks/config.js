065f80a071088186a9e06275e192aeaef3224722
Tony Trinh <tony19@gmail.com>
2017-01-08 19:13:32 -0600

    Move global.config to own file

---
+++ gulp-tasks/config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+global.config = {
+  polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
+  build: {
+    rootDirectory: 'build',
+    bundledDirectory: 'bundled',
+    unbundledDirectory: 'unbundled',
+    debugDirectory: 'debug',
+    // Accepts either 'bundled', 'unbundled', or 'both'
+    // A bundled version will be vulcanized and sharded. An unbundled version
+    // will not have its files combined (this is for projects using HTTP/2
+    // server push). Using the 'both' option will create two output projects,
+    // one for bundled and one for unbundled
+    bundleType: 'both'
+  },
+  // Path to your service worker, relative to the build root directory
+  serviceWorkerPath: 'service-worker.js',
+  // Service Worker precache options based on
+  // https://github.com/GoogleChrome/sw-precache#options-parameter
+  swPrecacheConfig: {
+    navigateFallback: '/index.html'
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -10,39 +10,14 @@
 
 'use strict';
 
-const path = require('path');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const lazypipe = require('lazypipe');
+require('./gulp-tasks/config.js');
 
 // Got problems? Try logging 'em
 // const logging = require('plylog');
 // logging.setVerbose();
-
-// !!! IMPORTANT !!! //
-// Keep the global.config above any of the gulp-tasks that depend on it
-global.config = {
-  polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
-  build: {
-    rootDirectory: 'build',
-    bundledDirectory: 'bundled',
-    unbundledDirectory: 'unbundled',
-    debugDirectory: 'debug',
-    // Accepts either 'bundled', 'unbundled', or 'both'
-    // A bundled version will be vulcanized and sharded. An unbundled version
-    // will not have its files combined (this is for projects using HTTP/2
-    // server push). Using the 'both' option will create two output projects,
-    // one for bundled and one for unbundled
-    bundleType: 'both'
-  },
-  // Path to your service worker, relative to the build root directory
-  serviceWorkerPath: 'service-worker.js',
-  // Service Worker precache options based on
-  // https://github.com/GoogleChrome/sw-precache#options-parameter
-  swPrecacheConfig: {
-    navigateFallback: '/index.html'
-  }
-};
 
 // Add your own custom gulp tasks to the gulp-tasks directory
 // A few sample tasks are provided for you
