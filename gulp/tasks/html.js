2c396093148dfd311d5803a239ebbb795c536ac0
Tony Trinh <tony19@gmail.com>
2017-01-09 10:20:49 -0600

    Refactor 'html' task to use classes

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -4,75 +4,86 @@ const Project = require('../project');
 const lazypipe = require('lazypipe');
 require('./htmllint');
 
-let project;
+class HtmlTask {
+  constructor() {
+    this.project = new PolymerProjectHelper();
+  }
+
+  build() {
+    const mergeTask = this.project.merge(
+      this.splitSource.bind(this),
+      this.splitDependencies.bind(this)
+    );
+    return mergeTask.call(this.project);
+  }
+
+  /**
+   * Splits all of your source files into one big ReadableStream. Source files
+   * are those in src/** as well as anything added to the `sources` property
+   * of polymer.json. Because most HTML Imports contain inline CSS and JS,
+   * those inline resources will be split out into temporary files. You can use
+   * $.if to filter files out of the stream and run them through specific
+   * tasks.
+   */
+  splitSource() {
+    const tsProject = $.typescript.createProject('tsconfig.json');
+    const tsPipe = lazypipe()
+      .pipe(tsProject)
+      .pipe($.uglify)
+      .pipe(() => $.if((file) => {
+          // Since TypeScript transpiles *.ts into *.js, we need to rename it
+          // back to *.ts so that the rejoiner can find it to reinsert into the
+          // original HTML file.
+          //
+          // The temporary filenames from the HTML splitter follow this pattern:
+          // <orig_html_file_basename>.html_script_<index>.js
+          // e.g., my-view1.html_script_1.js
+          return /\.html_script/i.test(file.basename);
+        },
+        $.extReplace('.ts')));
+
+    return this.project.splitSource()
+      .pipe($.debug({title: 'html:src'}))
+      .pipe($.if('**/*.css', $.cleanCss()))
+      .pipe($.if('**/*.html', $.htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true
+      })))
+
+      // Replace <script type="text/x-typescript"> into <script>
+      // since the script body gets transpiled into JavaScript
+      .pipe($.if('**/*.html', $.replace(/(<script.*type=["'].*\/)x-typescript/, '$1javascript')))
+
+      .pipe($.if('**/*.ts', tsPipe()))
+      .pipe($.if('**/*.js', $.babel({presets: ['es2015']})))
+      .pipe($.if(['**/*.js', '!**/*.min.js'], $.uglify()))
+      .pipe(this.project.rejoin());
+  }
+
+  /**
+   * Splits all of your dependency files into one big ReadableStream.
+   * These are determined from walking your source files and from the
+   * `extraDependencies` property of polymer.json.
+   */
+  splitDependencies() {
+    return this.project.splitDependencies()
+      .pipe($.debug({title: 'html:dep'}))
+      .pipe($.if('**/*.css', $.cleanCss()))
+      .pipe($.if('**/*.html', $.htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true
+      })))
+      .pipe($.if(['**/*.js', '!**/system*.js'], $.babel({presets: ['es2015']})))
+      .pipe($.if(['**/*.js', '!**/*.min.js'], $.uglify()))
+      .pipe(this.project.rejoin());
+  }
+}
 
 function html() {
-  project = new Project.PolymerProjectHelper();
-  return project.merge(source, dependencies).call(project);
-}
-
-// The source task will split all of your source files into one
-// big ReadableStream. Source files are those in src/** as well as anything
-// added to the sourceGlobs property of polymer.json.
-// Because most HTML Imports contain inline CSS and JS, those inline resources
-// will be split out into temporary files. You can use $.if to filter files
-// out of the stream and run them through specific tasks.
-function source() {
-  const tsProject = $.typescript.createProject('tsconfig.json');
-  const tsPipe = lazypipe()
-    .pipe(tsProject)
-    .pipe($.uglify)
-    .pipe(() => $.if((file) => {
-        // Since TypeScript transpiles *.ts into *.js, we need to rename it
-        // back to *.ts so that the rejoiner can find it to reinsert into the
-        // original HTML file.
-        //
-        // The temporary filenames from the HTML splitter follow this pattern:
-        // <orig_html_file_basename>.html_script_<index>.js
-        // e.g., my-view1.html_script_1.js
-        return /\.html_script/i.test(file.basename);
-      },
-      $.extReplace('.ts')));
-
-  return project.splitSource()
-    .pipe($.debug({title: 'html:src'}))
-    .pipe($.if('**/*.css', $.cleanCss()))
-    .pipe($.if('**/*.html', $.htmlmin({
-      collapseWhitespace: true,
-      removeComments: true,
-      minifyCSS: true
-    })))
-
-    // Replace <script type="text/x-typescript"> into <script>
-    // since the script body gets transpiled into JavaScript
-    .pipe($.if('**/*.html', $.replace(/(<script.*type=["'].*\/)x-typescript/, '$1javascript')))
-
-    .pipe($.if('**/*.ts', tsPipe()))
-    .pipe($.if('**/*.js', $.babel({presets: ['es2015']})))
-    .pipe($.if(['**/*.js', '!**/*.min.js'], $.uglify()))
-    .pipe(project.rejoin());
-}
-
-// The dependencies task will split all of your bower_components files into one
-// big ReadableStream
-// You probably don't need to do anything to your dependencies but it's here in
-// case you need it :)
-function dependencies() {
-  return project.splitDependencies()
-    .pipe($.debug({title: 'html:dep'}))
-    .pipe($.if('**/*.css', $.cleanCss()))
-    .pipe($.if('**/*.html', $.htmlmin({
-      collapseWhitespace: true,
-      removeComments: true,
-      minifyCSS: true
-    })))
-    .pipe($.if(['**/*.js', '!**/system*.js'], $.babel({presets: ['es2015']})))
-    .pipe($.if(['**/*.js', '!**/*.min.js'], $.uglify()))
-    .pipe(project.rejoin());
+  return new HtmlTask().build();
 }
 
 gulp.task('html', gulp.series('htmllint', html));
-module.exports = {
-  html,
-  project
-};
+module.exports = html;
