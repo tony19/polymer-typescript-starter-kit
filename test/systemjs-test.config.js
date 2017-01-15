570bc901183e4326d71beb72199f9c7b8680c866
Tony Trinh <tony19@gmail.com>
2017-01-14 22:58:59 -0600

    Move my-view1 test into test/components/my-view1

--- test/systemjs-test.config.js
+++ test/systemjs-test.config.js
@@ -1,12 +1,12 @@
 (global => {
   // Map any paths encountered in `import` statements
   const map = {
-    scripts:                        '../build/debug/src/scripts',
-    components:                     '../build/debug/src/components',
+    scripts:                        '../../../build/debug/src/scripts',
+    components:                     '../../../build/debug/src/components',
   };
 
   System.config({
-    baseUrl: '../node_modules',
+    baseUrl: '../../../node_modules',
     defaultJSExtensions: true,
     map
   });
