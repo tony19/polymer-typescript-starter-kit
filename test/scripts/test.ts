f9a5447e801e9c6736e77fa34cadafdd651d09d0
Tony Trinh <tony19@gmail.com>
2017-01-31 01:04:22 -0600

    Replace mocha ref with import

--- test/scripts/test.ts
+++ test/scripts/test.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+import 'mocha';
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
